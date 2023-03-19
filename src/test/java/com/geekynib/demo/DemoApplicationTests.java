57e553c827ab53fb64cac710f28428c378c5f259
nibchee <nirbhaybansal28@gmail.com>
2023-03-19 06:11:01 +0530

    Spring REST CRUD QuickStart

---
+++ src/test/java/com/geekynib/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekynib.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
